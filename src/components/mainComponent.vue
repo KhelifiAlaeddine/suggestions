1364321e831a1b1dda4d5efc523c9c210f6a0dd1
KHELIFI Alaeddine (Consulting For ACCOR Hotels) <alaeddine.khelifi@consulting-for.accor.com>
2024-03-06 14:50:26 +0100

    initial commit

---
+++ src/components/mainComponent.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import { ref } from 'vue';
+  import messages from '../constants/contants.js';
+  //const messages = ["Message 1", "Message 2", "Message 3"];
+  let message = ref("toto");
+  function randomMessage() {
+    const randomIndex = Math.floor(Math.random() * this.messages.length);
+    message.value = messages[randomIndex];
+  }
+</script>
+
+<template>
+  <div class="card">
+    <button type="button" @click="randomMessage()">TEST</button>
+    <p>{{ message }}</p>
+  </div>
+</template>
